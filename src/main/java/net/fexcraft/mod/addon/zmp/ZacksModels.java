f3e9b65d34a62e61238239bf37e0316f63254a31
Ferdinand Calo <fercalo96@yahoo.de>
2021-08-03 18:54:09 +0200

    2.0

--- src/main/java/net/fexcraft/mod/addon/zmp/ZacksModels.java
+++ src/main/java/net/fexcraft/mod/addon/zmp/ZacksModels.java
@@ -9,6 +9,6 @@ public class ZacksModels {
 	
     public static final String MODID = "zmp";
     public static final String NAME = "\"Zack's Model Projects\"";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0";
     
 }
